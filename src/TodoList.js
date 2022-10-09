6ceccab16c6e6d00a71425a88ca326148e34c796
Aaro Miikkulainen <aaro@aaro-air.lan>
2022-10-09 21:00:48 +0300

    Lisätty ag-grid

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
 import Todotable from './TodoTable';
+import { useRef } from 'react';
 
 function TodoList() {
-  const [todo, setTodo] = useState({desc: '', date: ''});
+  const [todo, setTodo] = useState({desc: '', date: '', priority:''});
   const [todos, setTodos] = useState([]);
+  const gridRef = useRef();
 
   const addTodo = (event) => {
     event.preventDefault();
@@ -16,18 +21,47 @@ function TodoList() {
   }
 
   const deleteTodo = (index) => {
-    setTodos(todos.filter((todo, i) => i !== index))
+    if (gridRef.current.getSelectedNodes().length > 0) {
+      setTodos(todos.filter((todo, index) =>
+        index !== gridRef.current.getSelectedNodes()[0].childIndex))
+    } else {
+      alert("Select row first");
+    }
   }
+
+  const columns = [
+    {headerName: 'Date', field: 'date', sortable: true, filter: true, floatingFilter: true},
+    {headerName: 'Description', field: 'desc', sortable: true, filter: true, floatingFilter: true},
+    {headerName: 'Priority', field: 'priority', sortable: true, filter: true, floatingFilter: true,
+    cellStyle: params => params.value === "High" ? {color: 'red'} : {color: 'black'}},
+  ]
 
   return (
     <div className="App">
-      <form onSubmit={addTodo}>
         <p>Add todo:</p>
-        Description:<input type="text" name="desc" value={todo.desc} onChange={inputChanged} />
-        Date:<input type="text" name="date" value={todo.date} onChange={inputChanged} />
-        <input type="submit" value="Add" />
-      </form>
-      <Todotable todos={todos}/>
+        <input type="text" name="desc" value={todo.desc} onChange={inputChanged} />
+        <input type="text" name="date" value={todo.date} onChange={inputChanged} />
+        <input type="text" name="priority" value={todo.priority} onChange={inputChanged} />
+        <button onClick={addTodo}>Add</button>
+        <button onClick={deleteTodo}>Delete</button>
+
+      <div
+        className="ag-theme-material"
+        style={{
+          height: '700px',
+          width: '50%',
+          margin: 'auto'}}
+        >
+          <AgGridReact
+            animateRows="true"
+            ref={gridRef}
+            onGridReady={params => gridRef.current = params.api}
+            rowSelection="single"
+            columnDefs={columns}
+            rowData={todos}
+          >
+          </AgGridReact>
+      </div>
     </div>
   );
 }
--- src/TodoTable.js
+++ src/TodoTable.js
@@ -4,22 +4,6 @@ function TodoTable(props) {
     
     return (
         <div>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Date</th>
-                        <th>Description</th>
-                    </tr>
-                    {
-                        props.todos.map((todo, index) =>
-                            <tr key={index}>
-                                <td>{todo.date}</td>
-                                <td>{todo.desc}</td>
-                                <td><button onClick={() => deleteTodo(index)}>Delete</button></td>
-                            </tr>
-                        )}
-                </tbody>
-            </table>
         </div>
     )
 }
